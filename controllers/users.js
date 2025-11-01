2ec46473150483e5ed3e1ae9105563695661773f
darshan02parmar <darshan02parmar@gmail.com>
2025-11-01 14:44:04 +0530

    Fix authentication flow - proper redirects, flash messages, and navbar behavior

--- controllers/users.js
+++ controllers/users.js
@@ -19,7 +19,7 @@ module.exports.signup=(async(req,res,next)=>{
         console.log(registeredUser);
         req.login(registeredUser,err=>{
             if(err) return next(err);
-            req.flash("success","Welcome to Wanderlust");
+            req.flash("success","Successfully signed up!");
             res.redirect("/listings");
         });
     }catch(e){
@@ -33,7 +33,7 @@ module.exports.renderLoginForm= (req, res) => {
 }
 
 module.exports.login=(req, res) => {
-  req.flash("success", "Welcome back!");
+  req.flash("success", "Successfully logged in!");
   let redirectUrl = res.locals.redirectUrl || "/listings";
   delete req.session.redirectUrl;
   res.redirect(redirectUrl);
